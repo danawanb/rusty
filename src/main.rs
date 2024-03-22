a9b08fcf50823f17bdd075b7f2cc32325df29ee8
danawan <[danawanbimantoro23@gmail.com]>
2024-03-22 14:51:37 +0700

    add person

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,27 @@ fn main() {
     for j in x {
         println!("{j}");
     }
+
+    let p = Person {
+        name: String::from("Danawan"),
+        age: 1000,
+    };
+
+    println!("{:?}", p);
+}
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: i32,
+}
+
+impl Person {
+    fn get_name(self) -> String {
+        self.name
+    }
+
+    fn print_age(&self) {
+        println!("the age is {}", self.age);
+    }
 }
