81bee5d3d1eb2fc46f75b7fef466b37ba6ded354
danawan <danawanbimantoro23@gmail.com>
2024-03-29 13:34:13 +0700

    init rust axum

--- src/main.rs
+++ src/main.rs
@@ -1,131 +1,130 @@
-#[derive(Debug)]
-struct Rectangle {
-    x: i32,
-    y: i32,
+use axum:: {
+    extract::State, http::StatusCode, response::{Html, IntoResponse, Response}, routing::{get, post}, Form, Json, Router
+};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::sync::Arc;
+
+use sqlx::mysql::{MySqlPool, MySqlPoolOptions};
+
+
+pub struct AppState {
+    db: MySqlPool,
+
 }
 
-impl Rectangle {
-    fn luas(&self) -> i32 {
-        self.x * self.y
-    }
+#[tokio::main]
+async fn main() {
+    let pool = MySqlPoolOptions::new()
+        .max_connections(100)
+        .connect("mysql://root:@localhost:3306/test").await.unwrap();
 
-    fn keliling(&self) -> i32 {
-        2 * (self.x + self.y)
-    }
 
-    fn double(&mut self) {
-        self.x *= 2;
-        self.y *= 2;
+    let app = Router::new()
+        .route("/api/healthchecker", get(get_foo))
+        .route("/bar", get(foo_bar))
+        .route("/insert_user", get(show_form))
+        .route("/do_insert", post(create_user))
+        .with_state(Arc::new(AppState { db: pool.clone() }));
+
+    // run our app with hyper, listening globally on port 3000
+    
+    println!("🚀 Server started successfully");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn get_foo() {
+
+}
+
+#[derive(sqlx::FromRow, Serialize, Debug, Deserialize)]
+#[allow(dead_code)]
+struct User {
+    user : String,
+    email : String
+}
+
+
+async fn foo_bar(
+    State(data): State<Arc<AppState>>,
+) -> impl IntoResponse {
+    
+    let result = sqlx::query_as::<_, User>("SELECT user, email FROM users LIMIT 1")
+    .fetch_one(&data.db)
+    .await;
+
+    match result {
+        Ok(x) => Json(json!(x)),
+        Err(y) => Json(json!({
+            "error" : true,
+            "message" : y.to_string(),
+        })),
     }
 }
 
-#[derive(Debug)]
-enum Color {
-    Red,
-    Green,
-    Blue,
-}
+// #[debug_handler]
+// fn insert_user(Form(user): Form<User>) -> (StatusCode, Json<User>) {
+//     (StatusCode::CREATED, Json(payload))
+// }
 
-fn main() {
-    let recc = Rectangle { x: 2, y: 2 };
-    let mut new_color: Vec<Rectangle> = Vec::new();
-    new_color.push(recc);
-    let rec2 = Rectangle { x: 6, y: 6 };
-    new_color.push(rec2);
-    println!("{:?}", new_color);
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `User` type
+    Json(payload): Json<User>,
+) -> (StatusCode, Json<User>) {
+    // insert your application logic here
 
-    let x = vec![1, 2, 3, 5];
-
-    for j in x {
-        println!("{j}");
-    }
-
-    let p = Person {
-        name: String::from("Danawan"),
-        age: 1000,
-    };
-
-    let mut manage: Vec<Person> = Vec::new();
-
-    manage.push(p);
-
-    let c = Company {
-        name: "Tkpedei".to_string(),
-        company_type: CompanyType::Tbk,
-        management: manage,
-    };
-
-    println!("{}", c.is_tbk());
-
-    let mut kotak = Rectangle { x: 6, y: 7 };
-    kotak.double();
-
-    println!(
-        "{:?} luas : {:?}, keliling : {:?}",
-        kotak,
-        kotak.luas(),
-        kotak.keliling()
-    );
-
-    let ccolor = Color::Blue;
-    println!("{:?}", ccolor);
-}
-
-#[derive(Debug)]
-struct Person {
-    name: String,
-    age: i32,
-}
-
-impl Person {
-    fn get_name(self) -> String {
-        self.name
-    }
-
-    fn print_age(&self) {
-        println!("the age is {}", self.age);
-    }
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(payload))
 }
 
 #[derive(Debug)]
-struct Company {
-    name: String,
-    company_type: CompanyType,
-    management: Vec<Person>,
+enum AuthError {
+    WrongCredentials,
+    MissingCredentials,
+    TokenCreation,
+    InvalidToken,
 }
 
-#[derive(Debug)]
-enum CompanyType {
-    Inc,
-    Gmbh(String),
-    Ltd,
-    Tbk,
+async fn show_form() -> Html<&'static str> {
+    Html(
+        r#"
+        <!doctype html>
+        <html>
+            <head></head>
+            <body>
+                <form action="/do_insert" method="post">
+                    <label for="name">
+                        Enter your name:
+                        <input type="text" name="name">
+                    </label>
+
+                    <label>
+                        Enter your email:
+                        <input type="text" name="email">
+                    </label>
+
+                    <input type="submit" value="Subscribe!">
+                </form>
+            </body>
+        </html>
+        "#,
+    )
 }
 
-trait Tbk {
-    fn is_tbk(&self) -> bool;
-    fn company_name(&self) -> String;
-    fn print_management(&self);
-    fn print_gmbh(&self);
-}
-
-impl Tbk for Company {
-    fn is_tbk(&self) -> bool {
-        match self.company_type {
-            CompanyType::Inc => false,
-            CompanyType::Ltd => false,
-            CompanyType::Gmbh(_) => false,
-            CompanyType::Tbk => true,
-        }
+impl IntoResponse for AuthError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            AuthError::WrongCredentials => (StatusCode::UNAUTHORIZED, "Wrong credentials"),
+            AuthError::MissingCredentials => (StatusCode::BAD_REQUEST, "Missing credentials"),
+            AuthError::TokenCreation => (StatusCode::INTERNAL_SERVER_ERROR, "Token creation error"),
+            AuthError::InvalidToken => (StatusCode::BAD_REQUEST, "Invalid token"),
+        };
+        let body = Json(json!({
+            "error": error_message,
+        }));
+        (status, body).into_response()
     }
-
-    fn company_name(&self) -> String {
-        self.name.clone()
-    }
-
-    fn print_management(&self) {
-        println!("{:?}", self.management);
-    }
-
-    fn print_gmbh(&self) {}
 }
